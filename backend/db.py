540f5dbf24bbf9d3366a01b0a2efd3309c2eb089
Sesa189 <cesare.nappa@fermi.mo.it>
2026-01-17 10:01:20 +0100

    Aggiunti dockerfile

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,12 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo import AsyncMongoClient
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 DB_NAME = "tournament"
 COOKIE_SECRET = "super_secret_key_change_me"
 PORT = 8888
 
-#client = AsyncIOMotorClient(MONGO_URL)
-client = AsyncMongoClient(MONGO_URL)
+client = AsyncIOMotorClient(MONGO_URL)
 db = client[DB_NAME]
 
 matches = db["matches"]
